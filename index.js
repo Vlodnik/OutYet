0007396f08911602aab7eefb8482f9d169201de7
Evan Thompson <evanbrowningthompson@gmail.com>
2017-12-03 12:38:44 -0500

    changed app parameters to just tv shows, updated html, added getJSON function in js

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 'use strict'
 
+function getDataFromApi(userInput, callback) {
+	const endpointURL = 'http://api.tvmaze.com/search/shows';
+
+	const searchObject = {
+		q: `${userInput}`
+	};
+
+	$.getJSON(endpointURL, searchObject, callback)
+}
+
+function displayApiData(data) {
+	console.log(data);
+}
+
 function handleSubmitButton() {
 	$('#js-search').click(function(event) {
 		event.preventDefault();
+
+		console.log('handleSubmitButton ran');
 
 		const searchField = $(this).siblings('#js-input');
 		const userInput = searchField.val();
