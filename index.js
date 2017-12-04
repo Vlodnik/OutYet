78a9ae0cae6d7b7dddb81a6950249d8a59a77fa6
Evan Thompson <evanbrowningthompson@gmail.com>
2017-12-03 19:21:50 -0500

    added initial results display page

--- index.js
+++ index.js
@@ -4,14 +4,49 @@ function getDataFromApi(userInput, callback) {
 	const endpointURL = 'http://api.tvmaze.com/search/shows';
 
 	const searchObject = {
-		q: `${userInput}`
+		q: `${ userInput }`
 	};
 
 	$.getJSON(endpointURL, searchObject, callback)
 }
 
 function displayApiData(data) {
-	console.log(data);
+	console.log(data); // testing
+	console.log(data[0].show._links.previousepisode.href); // testing
+
+	const results = data.map(item => renderResults(item));
+
+	const resultsDiv = $('#js-results');
+
+	resultsDiv
+		.html(results)
+		.prop('hidden', false);
+}
+
+function renderResults(data) {
+	const show = data.show;
+
+	if(show.image) {
+		return `
+			<section class="result-show">
+				<h2>${ show.name }</h2>
+				<a href="${ show.officialSite }" target="_blank">
+					<img src="${ show.image.medium }" alt="${ show.name }">
+				</a>
+				<button class="js-confirm" type="submit">Is this your show?</button>
+			</section>
+		`;
+	} else {
+		return `
+			<section class="result-show">
+				<h2>${ show.name }</h2>
+				<a href="${ show.officialSite }" target="_blank">
+					<img src="https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg" alt="${ show.name }">
+				</a>
+				<button class="js-confirm" type="submit">Is this your show?</button>
+			</section>
+		`;
+	}
 }
 
 function handleSubmitButton() {
